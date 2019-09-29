c12d38c8b0e7f977fb16048953e869da3b107acc
Dandy <benggaboo1@gmail.com>
2019-09-29 11:25:47 +0800

    final push

--- src/main/java/com/uhack/constructify/mapper/OrderRequestMapper.java
+++ src/main/java/com/uhack/constructify/mapper/OrderRequestMapper.java
@@ -3,6 +3,7 @@ package com.uhack.constructify.mapper;
 import com.uhack.constructify.model.domain.OrderRequest;
 import com.uhack.constructify.model.dto.PendingRequestResponse;
 import com.uhack.constructify.model.entity.OrderRequestEntity;
+import org.aspectj.weaver.ast.Or;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -28,5 +29,10 @@ public interface OrderRequestMapper {
     default void removeParent(OrderRequestEntity entity, @MappingTarget OrderRequest domain) {
         entity.getArea().setProject(null);
         entity.setProject(null);
+    }
+
+    @AfterMapping
+    default void removeParent(List<OrderRequest> entityList, @MappingTarget List<OrderRequest> domainList) {
+        entityList.forEach(entity -> entity.getArea().setProject(null));
     }
 }
